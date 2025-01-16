1f58739a764a43d6f00157b6de8313d2fa0de302
glenn <glennchiang24@gmail.com>
2025-01-16 16:53:34 +0800

    Create GameBanner component

--- src/data/games.ts
+++ src/data/games.ts
@@ -6,8 +6,12 @@ export interface Game {
 
 export const GAMES: Game[] = [
   { id: "coin-toss", name: "Coin Toss", description: "description" },
-  { id: "dice-roll", name: "Dice Roll", description: "description"},
+  { id: "dice-roll", name: "Dice Roll", description: "description" },
   { id: "spin-the-wheel", name: "Spin the Wheel", description: "description" },
-  { id: "reaction-test", name: "Reaction Test", description: "description"},
-  { id: "stat-challenge", name: "Stat Challenge", description: "description"},
+  {
+    id: "reaction-test",
+    name: "Reaction Test",
+    description: "When the blue box turns red, click as fast as you can",
+  },
+  { id: "stat-challenge", name: "Stat Challenge", description: "description" },
 ];
