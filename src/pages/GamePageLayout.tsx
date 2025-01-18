dd3ae8649de03d90ebcd4ed579ad2574020462b8
glenn <glennchiang24@gmail.com>
2025-01-18 14:21:31 +0800

    Add BackButton

--- src/pages/GamePageLayout.tsx
+++ src/pages/GamePageLayout.tsx
@@ -1,10 +1,15 @@
+import { Box } from "@mui/material";
 import { Outlet } from "react-router";
+import { BackButton } from "../components/BackButton";
 import { ConfettiProvider } from "../components/ConfettiProvider";
 
 export default function GamePageLayout() {
   return (
     <ConfettiProvider>
-      <Outlet />
+      <Box display={"flex"} flexDirection={"column"} alignItems={"start"}>
+        <BackButton />
+        <Outlet />
+      </Box>
     </ConfettiProvider>
   );
 }
