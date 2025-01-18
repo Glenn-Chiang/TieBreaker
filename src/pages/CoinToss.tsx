422e6e7f84a3898e09b03cc103937f30ca7d53ec
Wang Weijie <weijiewang227@gmail.com>
2025-01-18 11:37:49 +0800

    finish coinflip

--- src/pages/CoinToss.tsx
+++ src/pages/CoinToss.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { useRef, useState } from "react";
 import GameBanner from "../components/GameBanner";
 import { GAMES } from "../data/games";
 import headVideo from "../assets/heads_flip.mp4";
@@ -6,11 +7,34 @@ import tailVideo from "../assets/tails_flip.mp4";
 
 export default function CoinToss() {
   const gameData = GAMES.find((data) => data.id === "coin-toss")!;
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [video, setVideo] = useState(headVideo);
+
+  const onFlip = () => {
+    if (Math.random() > 0.5) {
+      setVideo(tailVideo);
+    } else {
+      setVideo(headVideo);
+    }
+
+    if (videoRef.current) {
+      videoRef.current.pause(); // Pause the current video
+      videoRef.current.src = video; // Update the video source directly
+      videoRef.current.load();
+
+      videoRef.current.play();
+    }
+  };
 
   return (
     <>
       <GameBanner gameData={gameData} />
-      <Box></Box>
+      <Box>
+        <Button onClick={onFlip}>Flip</Button>
+        <video ref={videoRef}>
+          <source src={video} />
+        </video>
+      </Box>
     </>
   );
 }
