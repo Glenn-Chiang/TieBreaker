4d3f731907ecd90528241d7ca75125d7c29ddc3f
Wang Weijie <weijiewang227@gmail.com>
2025-01-18 11:15:41 +0800

    imported cointoss animations

--- src/pages/CoinToss.tsx
+++ src/pages/CoinToss.tsx
@@ -1,17 +1,16 @@
 import { Box } from "@mui/material";
 import GameBanner from "../components/GameBanner";
 import { GAMES } from "../data/games";
+import headVideo from "../assets/heads_flip.mp4";
+import tailVideo from "../assets/tails_flip.mp4";
 
 export default function CoinToss() {
-  const gameData = GAMES.find(data => data.id === "coin-toss")!
+  const gameData = GAMES.find((data) => data.id === "coin-toss")!;
 
   return (
     <>
-      <GameBanner gameData={gameData}/>
-      <Box>
-
-      </Box>
-
+      <GameBanner gameData={gameData} />
+      <Box></Box>
     </>
-  )
+  );
 }
