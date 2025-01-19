01383559685db8dd29ab8ab053a978951575239f
glenn <glennchiang24@gmail.com>
2025-01-19 10:25:56 +0800

    Fix assets path

--- src/data/games.ts
+++ src/data/games.ts
@@ -11,27 +11,27 @@ export const GAMES: Game[] = [
     id: "coin-toss",
     name: "Coin Toss",
     description: "Quick and simple",
-    icon: "/src/assets/coin_icon.png",
+    icon: "/assets/coin_icon.png",
     maxPlayers: 2,
   },
   {
     id: "dice-roll",
     name: "Dice Roll",
     description: "Highest roll wins",
-    icon: "/src/assets/dice_icon.png",
+    icon: "/assets/dice_icon.png",
     maxPlayers: 2,
   },
   {
     id: "spin-the-wheel",
     name: "Spin the Wheel",
     description: "Use the + and - buttons to adjust the number of players",
-    icon: "/src/assets/wheel_icon.png",
+    icon: "/assets/wheel_icon.png",
     maxPlayers: 6,
   },
   {
     id: "reaction-test",
     name: "Reaction Test",
-    icon: "/src/assets/bolt_icon.webp",
+    icon: "/assets/bolt_icon.webp",
     description:
       "When the blue buttons turns green, click as fast as you can. If you click early, you lose immediately.",
     maxPlayers: 2,
@@ -39,7 +39,7 @@ export const GAMES: Game[] = [
   {
     id: "click-test",
     name: "Clicking Test",
-    icon: "/src/assets/cursor_icon.webp",
+    icon: "/assets/cursor_icon.webp",
     description:
       "Click the button as many times as you can before the timer runs out",
     maxPlayers: 2,
@@ -48,28 +48,28 @@ export const GAMES: Game[] = [
     id: "stat-challenge",
     name: "Stats Challenge",
     description: "Click the button to generate a random question. Whoever has the highest stats wins!",
-    icon: "/src/assets/stats_icon.png",
+    icon: "/assets/stats_icon.png",
     maxPlayers: null,
   },
   {
     id: "balloon-pop",
     name: "Balloon Pop",
     description: "Take turns to pump the balloon until it pops",
-    icon: "/src/assets/balloon_icon.png",
+    icon: "/assets/balloon_icon.png",
     maxPlayers: null
   },
   {
     id: "pen14",
     name: "Pen14",
     description: "In a public space, choose a word, take turns to say the word with increasing volume, first one who chickens out loses",
-    icon: "/src/assets/pen_icon.png",
+    icon: "/assets/pen_icon.png",
     maxPlayers: null
   },
   {
     id: "typing-test",
     name: "Typing Test",
     description: "Type as many words as you can within the time limit",
-    icon: "/src/assets/keyboard_icon.png",
+    icon: "/assets/keyboard_icon.png",
     maxPlayers: null
   }
 ];
